969b449eb679bb2c81c3059cf7c32f14c821698b
Brandon Asheim <brandonasheim@gmail.com>
2023-01-24 20:31:21 -0800

    feat(): add authentication

--- app.py
+++ app.py
@@ -15,16 +15,16 @@ def health():
 @app.post("/py/api/activate")
 @auth.login_required()
 def activate():
-    pass
+    return {"status": "activation_completed"}
 
 
 @app.post("/py/api/deactivate")
 @auth.login_required()
 def deactivate():
-    pass
+    return {"status": "deactivation_completed"}
 
 
 @auth.verify_password
 def verify_password(username, password):
-    secret_json = json.loads(get_secret(Secret.DB)["SecretString"])
+    secret_json = json.loads(get_secret(Secret.PASSWORD)["SecretString"])
     return username == secret_json["user"] and password == secret_json["password"]
