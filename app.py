c23c264bcd9812e169848a95f42ff1b89796e428
Brandon Asheim <brandonasheim@gmail.com>
2023-01-26 16:55:31 -0800

    fix(): log to gunicorn

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ from lib.clients.secrets_manager import get_secret, Secret
 from lib.clients.alpaca_manager import get_current_market_price
 from lib.auto_trader.schedule import activate, deactivate, active_jobs
 import json
+import logging
 
 auth = HTTPBasicAuth()
 app = Flask(__name__)
+
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
 
 @app.get("/py/api/health")
