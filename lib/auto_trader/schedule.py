44961aa3b699bb7d5972e11fee3cbfc349aa1b38
Brandon Asheim <brandonasheim@gmail.com>
2023-02-21 10:24:13 -0800

    feat(): update monitor to use correct stock dates

--- lib/auto_trader/schedule.py
+++ lib/auto_trader/schedule.py
@@ -6,6 +6,9 @@ from lib.auto_trader.v3.manager import orchestrator, monitor, opening_price
 from lib.clients.backend_manager import get_stocks_backend
 from lib.misc.plant_refresh import refresh_plants
 from flask import current_app
+from pandas_market_calendars import get_calendar, date_range
+from datetime import datetime, timedelta
+from pytz import timezone
 
 scheduler = BackgroundScheduler()
 active_jobs: [Job] = []
@@ -16,16 +19,19 @@ def activate(app, ml_models):
     with_function(app, ml_models.build_models)
     with_function(app, opening_price, tuple([ml_models]))
     active_jobs.append(
-        scheduler.add_job(lambda: with_function(app, ml_models.build_models), CronTrigger.from_crontab('0 13 * * mon-fri', 'utc'), replace_existing=True)
+        scheduler.add_job(lambda: stock_wrapper(app, ml_models.build_models), CronTrigger.from_crontab('0 13 * * mon-fri', 'utc'), replace_existing=True)
     )
     active_jobs.append(
-        scheduler.add_job(lambda: with_function(app, orchestrator, tuple([ml_models])), CronTrigger.from_crontab('0 15 * * mon-fri', 'utc'), replace_existing=True)
+        scheduler.add_job(lambda: stock_wrapper(app, orchestrator, tuple([ml_models])), CronTrigger.from_crontab('0 15 * * mon-fri', 'utc'), replace_existing=True)
     )
     active_jobs.append(
-        scheduler.add_job(lambda: with_function(app, opening_price, tuple([ml_models])), CronTrigger.from_crontab('40 14 * * mon-fri', 'utc'), replace_existing=True)
+        scheduler.add_job(lambda: stock_wrapper(app, opening_price, tuple([ml_models])), CronTrigger.from_crontab('33 14 * * mon-fri', 'utc'), replace_existing=True)
     )
     active_jobs.append(
-        scheduler.add_job(lambda: with_function(app, monitor, tuple([ml_models])), CronTrigger.from_crontab('30 15-20 * * mon-fri', 'utc'), replace_existing=True)
+        scheduler.add_job(lambda: stock_wrapper(app, monitor, tuple([ml_models])), CronTrigger.from_crontab('15,30,45 15-20 * * mon-fri', 'utc'), replace_existing=True)
+    )
+    active_jobs.append(
+        scheduler.add_job(lambda: stock_wrapper(app, monitor, tuple([ml_models])), CronTrigger.from_crontab('45 14 * * mon-fri', 'utc'), replace_existing=True)
     )
 
 
@@ -75,3 +81,17 @@ def with_function(app, func, passed_args=tuple()):
     with app.app_context():
         current_app.logger.info(f"Background function: ${func.__name__}")
         func(*passed_args)
+
+
+def stock_wrapper(app, func, passed_args=tuple()):
+    d1 = datetime.today() - timedelta(days=1)
+    d2 = datetime.today() + timedelta(days=1)
+    nyse = get_calendar('NYSE')
+    open_date_range = nyse.schedule(start_date=d1, end_date=d2)
+    dates = date_range(open_date_range, frequency='1D')
+    now = datetime.now(tz=timezone('UTC'))
+    for date in dates:
+        converted_date = datetime.fromisoformat(date)
+        if converted_date.day == now.day and now < converted_date:
+            with_function(app, func, passed_args)
+            break
